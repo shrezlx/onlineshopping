c604e114834eda0b8f6918341ac247ceda6aea91
shrezlx <shrezlx@gmail.com>
2017-07-03 17:08:10 -0400

    Completed the interfacetheme

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -11,9 +11,37 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
+	
+	
+	@RequestMapping(value ="/about" )
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/*
 	 * @RequestMapping(value="/test") public ModelAndView
 	 * test(@RequestParam(value = "greeting", required = false)String greeting){
